415a9a4cb8ef30a3eb44a8eb37682083631d9bf0
JAESURO <amantaiulyzhandos2005@gmail.com>
2025-02-25 12:00:02 +0500

    6

--- src/repository/mongodb_repo.rs
+++ src/repository/mongodb_repo.rs
@@ -3,8 +3,8 @@ extern crate dotenv;
 use dotenv::dotenv;
 
 use mongodb::{
-    bson::{extjson::de::Error, oid::ObjectId, doc},
-    results::{ InsertOneResult, UpdateResult, DeleteResult},
+    bson::{oid::ObjectId, doc},
+    results::{InsertOneResult, UpdateResult, DeleteResult},
     Client, Collection,
 };
 use futures::stream::TryStreamExt;
@@ -17,93 +17,54 @@ pub struct MongoRepo {
 impl MongoRepo {
     pub async fn init() -> Self {
         dotenv().ok();
-        let uri = match env::var("MONGOURI") {
-            Ok(v) => v.to_string(),
-            Err(_) => format!("Error loading env variable"),
-        };
-        let client = Client::with_uri_str(uri).await.unwrap();
+        let uri = env::var("MONGOURI").expect("MONGOURI must be set");
+        let client = Client::with_uri_str(&uri).await.expect("Failed to initialize MongoDB client");
         let db = client.database("rustDB");
         let col: Collection<User> = db.collection("User");
         MongoRepo { col }
     }
 
-    pub async fn create_user(&self, new_user: User) -> Result<InsertOneResult, Error> {
+    pub async fn create_user(&self, new_user: User) -> Result<InsertOneResult, mongodb::error::Error> {
         let new_doc = User {
             id: None,
             name: new_user.name,
             location: new_user.location,
             title: new_user.title,
         };
-        let user = self
-            .col
-            .insert_one(new_doc, None)
-            .await
-            .ok()
-            .expect("Error creating user");
-        Ok(user)
+        self.col.insert_one(new_doc, None).await
     }
 
-    pub async fn get_user(&self, id: &String) -> Result<User, Error> {
-        let obj_id = ObjectId::parse_str(id).unwrap();
-        let filter = doc! {"_id": obj_id};
-        let user_detail = self
-            .col
-            .find_one(filter, None)
-            .await
-            .ok()
-            .expect("Error getting user's detail");
-        Ok(user_detail.unwrap())
+    pub async fn get_user(&self, id: &String) -> Result<Option<User>, mongodb::error::Error> {
+        let obj_id = ObjectId::parse_str(id)?;
+        let filter = doc! { "_id": obj_id };
+        self.col.find_one(filter, None).await
     }
 
-    pub async fn update_user(&self, id: &String, new_user: User) -> Result<UpdateResult, Error> {
-        let obj_id = ObjectId::parse_str(id).unwrap();
-        let filter = doc! {"_id": obj_id};
+    pub async fn update_user(&self, id: &String, new_user: User) -> Result<UpdateResult, mongodb::error::Error> {
+        let obj_id = ObjectId::parse_str(id)?;
+        let filter = doc! { "_id": obj_id };
         let new_doc = doc! {
-            "$set":
-                {
-                    "id": new_user.id,
-                    "name": new_user.name,
-                    "location": new_user.location,
-                    "title": new_user.title
-                },
+            "$set": {
+                "name": new_user.name,
+                "location": new_user.location,
+                "title": new_user.title,
+            },
         };
-        let updated_doc = self
-            .col
-            .update_one(filter, new_doc, None)
-            .await
-            .ok()
-            .expect("Error updating user");
-        Ok(updated_doc)
+        self.col.update_one(filter, new_doc, None).await
     }
 
-    pub async fn delete_user(&self, id: &String) -> Result<DeleteResult, Error> {
-        let obj_id = ObjectId::parse_str(id).unwrap();
-        let filter = doc! {"_id": obj_id};
-        let user_detail = self
-            .col
-            .delete_one(filter, None)
-            .await
-            .ok()
-            .expect("Error deleting user");
-        Ok(user_detail)
+    pub async fn delete_user(&self, id: &String) -> Result<DeleteResult, mongodb::error::Error> {
+        let obj_id = ObjectId::parse_str(id)?;
+        let filter = doc! { "_id": obj_id };
+        self.col.delete_one(filter, None).await
     }
 
-    pub async fn get_all_users(&self) -> Result<Vec<User>, Error> {
-        let mut cursors = self
-            .col
-            .find(None, None)
-            .await
-            .ok()
-            .expect("Error getting list of users");
-        let mut users: Vec<User> = Vec::new();
-        while let Some(user) = cursors
-            .try_next()
-            .await
-            .ok()
-            .expect("Error mapping through cursor")
-        {
-            users.push(user)
+    pub async fn get_all_users(&self) -> Result<Vec<User>, mongodb::error::Error> {
+        let mut cursors = self.col.find(None, None).await?;
+        let mut users = Vec::new();
+        while let Some(user) = cursors.try_next().await? {
+            users.push(user);
         }
         Ok(users)
-        }
     }
+}
